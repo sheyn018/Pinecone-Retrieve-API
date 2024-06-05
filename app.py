57ca6080c9fb692ce4f92cfc72024a3af971ade7
sheyn018 <sheane39@gmail.com>
2024-06-05 03:18:51 -0400

    added scores in api response

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
@@ -8,7 +9,6 @@ from pinecone import Pinecone
 # Load environment variables
 load_dotenv(find_dotenv(), override=True)
 pinecone_api_key = os.getenv("PINECONE_API_KEY")
-openai_api_key = os.getenv("OPENAI_API_KEY")
 
 # Initialize Flask app and CORS
 app = Flask(__name__)
@@ -38,12 +38,14 @@ def retrieve():
 
     # Extract and format the matched documents
     matches = response['matches']
-    documents = [match['metadata']['content'] for match in matches]
+    
+    # Extract and format the score and content into a single list
+    formatted_matches = [{'score': match['score'], 'content': match['metadata']['content']} for match in matches]
 
-    formatted_documents = "\n".join([f"Chunk Reference {i}: {doc}" for i, doc in enumerate(documents, 1)])
+    print("Matches: ", formatted_matches)
 
     # Return the JSON response with a label
-    return jsonify({'chunks': formatted_documents}), 200
+    return jsonify({'chunks': formatted_matches}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
